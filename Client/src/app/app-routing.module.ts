742698d84524ff2f1793e9a7461f0cdc664747bb
JanGruber <jan_gruber@outlook.com>
2020-01-16 13:39:26 +0100

    add login page(not finished)

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MessagesComponent } from './components/messages/messages.component';
+import {LoginComponent} from './components/login/login.component';
+import {ChatOverviewComponent} from './components/chat-overview/chat-overview.component';
 
 
 const routes: Routes = [
-//  {path: '', component: MessagesComponent}
+  // { path: '',
+  //   redirectTo: '/login',
+  //   pathMatch: 'full'
+  // },
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'chat-overview', component: ChatOverviewComponent}
 ]
 
 @NgModule({
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './material/material.module';
 import { MessagesComponent } from './components/messages/messages.component';
 import { PusherService } from './services/pusher.service';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { ChatOverviewComponent } from './components/chat-overview/chat-overview.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent
+    MessagesComponent,
+    LoginComponent,
+    ChatOverviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CustomMaterialModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule
   ],
   providers: [PusherService],
   bootstrap: [AppComponent]
